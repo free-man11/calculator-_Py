0e5dc8fdb2250299816881faa473b06609c3faf0
Phinehas Freeman <98413109+free-man11@users.noreply.github.com>
2022-08-08 10:31:20 +0000

    Update my.py

--- my.py
+++ my.py
@@ -1,3 +1,7 @@
+#Write a program that produces the sum of all prime numbers less than a given number say n
+#Prime numbers are numbers that is divisible only by itself and 1 (e.g. 2, 3, 5, 7, 11).
+#So let's start with the coding.
+
 num = input("Enter the ending value")
 num = int(num)
 sum = 0
