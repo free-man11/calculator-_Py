cd65eed0cf3755b883a17db85d95bd09315157c0
Phinehas Freeman <98413109+free-man11@users.noreply.github.com>
2022-08-08 12:47:26 +0000

    Update my.py

--- my.py
+++ my.py
@@ -2,18 +2,25 @@
 #Prime numbers are numbers that is divisible only by itself and 1 (e.g. 2, 3, 5, 7, 11).
 #So let's start with the coding.
 
-num = input("Enter the ending value")
-num = int(num)
+
+lower = int(input("enter lower range: "))
+upper = int(input("enter upper range: "))
 sum = 0
+prime = 0
 
-#print numbers from 2 to n
-for i in range(2, num):
-    for j in range(2, i):
-#checks whether number i%j is equivalent to 0
-        if (i % j == 0):
+#print numbers from lower range to upper range a user will input
+for num in range(lower, upper + 1):
+    if num > 1:
+        
+#checks whether number num % j is equivalent to 0 and that will be even
+        for i in range(2, num):
+          if (num % i) == 0:
             break
-    else:
-        print(i)
-        sum = sum + i
 
-print("Sum of prime number is", sum)
+        else:
+            print(num)
+            sum = sum + num
+            prime = prime+1
+
+print("sum of prime numbers", sum)
+print("average of prime is",sum/num)
